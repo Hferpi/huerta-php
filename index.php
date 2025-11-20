6519a5c9db63796a2e9f77583fe23e57c7268bad
“Hferpi” <hferpi@gmail.com>
2025-11-20 21:09:26 +0100

    añadir estilos crear tablas formularios todo visual añadir menu añadir carpeta de imagenes y fuente

--- index.php
+++ index.php
@@ -2,30 +2,83 @@
 include 'conexion.php';
 $nameDB = 'huerta_db';
 $connect = connectDB($nameDB);
-
+if ($connect->connect_error)
+    die("Error: " . $connect->connect_error);
+$query = "Select id, nombre, tipo, dias_cosecha from cultivos;";
+$result = $connect->query($query);
 
 ?>
 
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Huerta ecologica</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <footer>
-        <h1>Huerta ecologica</h1>
-    </footer>
-    <hr>
+    <header>
+        <h1>🚜 Huerta ecologica 🍅</h1>
+
+        <button class="log-out">LogOut</button>
+    </header>
+
     <section>
-        
+        <div class="menu">
+            <h4>Menu Principal</h4>
+            <hr>
+            <div class="routs-menu">
+
+                <a href="index.php"><img src="./source/img/home.svg" alt="home">Home</a>
+
+                <a href="nuevo.php"><img src="./source/img/add.svg" alt="añadir">Añadir</a>
+
+
+            </div>
+
+        </div>
+        <div>
+            <div class="encabezado-tabla">
+                <h3>Resultados</h3>
+                <div class="ordenar">
+                    <span>Dias de cosecha: </span>
+                    <select name="order-by">
+                        <option value="desc">Desc</option>
+                        <option value="asc">Asc</option>
+                    </select>
+                    <button type="submit">Ordenar</button>
+                </div>
+            </div>
+            <?php if ($result && $result->num_rows > 0): ?>
+                <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Tipo</th>
+                        <th>Dias Cosecha</th>
+                    </tr>
+                    <?php while ($r = $result->fetch_assoc()) : ?>
+                        <tr>
+                            <td><?= $r['id'] ?></td>
+                            <td><?= $r['nombre'] ?></td>
+                            <td><?= $r['tipo'] ?></td>
+                            <td><?= $r['dias_cosecha'] ?></td>
+                        </tr>
+                    <?php endwhile; ?>
+                </table>
+            <?php else: ?>
+                <p>No hay registros.</p>
+            <?php endif; ?>
+        </div>
     </section>
-    <hr>
+
     <footer>
-        <p>Ejercio practicar PHP, 2CFGM profesora Lorena</p>
+        <p>© Ejercio practicar PHP, 2CFGM profesora Lorena</p>
     </footer>
 </body>
+
 </html>
--- nuevo.php
+++ nuevo.php
@@ -1,3 +1,80 @@
-<?php 
-echo("hola");
+<?php
+include 'conexion.php';
+$nameDB = 'huerta_db';
+$connect = connectDB($nameDB);
+if ($connect->connect_error)
+    die("Error: " . $connect->connect_error);
+$query = "Select id, nombre, tipo, dias_cosecha from cultivos;";
+$result = $connect->query($query);
+
 ?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Huerta ecologica</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <h1>🚜 Huerta ecologica 🍅</h1>
+
+        <button class="log-out">LogOut</button>
+    </header>
+
+    <section>
+        <div class="menu">
+            <h4>Menu Principal</h4>
+            <hr>
+            <div class="routs-menu">
+
+                <a href="index.php"><img src="./source/img/home.svg" alt="home">Home</a>
+
+                <a href="nuevo.php"><img src="./source/img/add.svg" alt="añadir">Añadir</a>
+
+
+            </div>
+
+        </div>
+        <div>
+            <div class="encabezado-tabla">
+                <h3>Añadir</h3>
+            </div>
+            <form action="post">
+                <div>
+                    <span>Nombre:</span>
+                    <input type="text" name="nombre" id="nomnbre-furta" placeholder="Nombre fruta">
+                </div>
+                <div>
+                    <span>Tipo de hortaliza / fruta: </span>
+                    <select name="tipo" id="tipo-fruta">
+                        <option value="hortaliza">Hortaliza</option>
+                        <option value="fruta">Fruta</option>
+                        <option value="aromatica">Aromatica</option>
+                        <option value="legumbre">Legumbre</option>
+                        <option value="tuberculo">Tuberculo</option>
+                    </select>
+                </div>
+                <div>
+                    <span>Dias para cosecha: </span>
+                    <input type="number" name="dias" id="dias-cosecha" placeholder="0" min="1" max="999">
+                </div>
+                <div>
+                    <input type="submit">
+                    <input type="reset" value="Limpiar">
+                </div>
+            </form>
+        </div>
+    </section>
+
+    <footer>
+        <p>© Ejercio practicar PHP, 2CFGM profesora Lorena</p>
+    </footer>
+</body>
+
+</html>
